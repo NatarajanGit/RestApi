91c5c9f4cf5bf611f30aac205fc64c216318b270
natarajan.easwaran <natarajan.easwaran@CPX-N6KICHA0I31.dir.svc.accenture.com>
2018-01-26 15:08:18 -0600

    Connecting to repository

--- RestApi/src/main/java/com/rest/api/repository/EmployeeRepository.java
+++ RestApi/src/main/java/com/rest/api/repository/EmployeeRepository.java
@@ -1,6 +1,10 @@
 package com.rest.api.repository;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 import com.rest.api.pojo.Employee;
@@ -10,37 +14,79 @@ public class EmployeeRepository {
 	private static List<Employee> empList = null;
 
 	public static List<Employee> getEmployeeDetails() {
-		return empList;
 
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=restdb", "root",
+					"");
+			System.out.println("connected");
+			Statement statement = conn.createStatement();
+
+			String sql = "select * from employee";
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+
+				Employee emp = new Employee();
+				int id = rs.getInt("empid");
+				String name = rs.getString("empname");
+
+				emp.setEmpId(id);
+				emp.setEmpName(name);
+
+				empList.add(emp);
+			}
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return empList;
 	}
 
 	public static Employee getEmployee(int id) {
-		for (Employee emp : empList) {
-			if (emp.getEmpId() == id) {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=restdb", "root",
+					"");
+			System.out.println("connected");
+			String sql = "select * from employee where empId = ?";
+			PreparedStatement statement = conn.prepareStatement(sql);
+
+			statement.setInt(1, id);
+
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+
+				Employee emp = new Employee();
+				int empId = rs.getInt("empid");
+				String empName = rs.getString("empname");
+
+				emp.setEmpId(empId);
+				emp.setEmpName(empName);
+
 				return emp;
 			}
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return null;
 	}
 
 	public static void createEmployee(Employee emp) {
-			empList.add(emp);
-	}
 
-	static {
-
-		empList = new ArrayList<Employee>();
-
-		Employee emp1 = new Employee();
-		emp1.setEmpId(101);
-		emp1.setEmpName("Vijay");
-
-		Employee emp2 = new Employee();
-		emp2.setEmpId(102);
-		emp2.setEmpName("Jagan");
-
-		empList.add(emp1);
-		empList.add(emp2);
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=restdb", "root",
+					"");
+			System.out.println("connected");
+			Statement statement = conn.createStatement();
+			int id = emp.getEmpId();
+			String name = emp.getEmpName();
+			statement.executeUpdate("insert into employee values(" + id + "," + name + ")");
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 }
