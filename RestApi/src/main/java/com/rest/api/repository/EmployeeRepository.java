9a1f33d543a0baddb3bf3977b7f4ddb6849c4248
natarajan <natarajan@10.0.0.46>
2018-01-28 20:08:28 -0600

    DB connectivity for HTTP methods

--- RestApi/src/main/java/com/rest/api/repository/EmployeeRepository.java
+++ RestApi/src/main/java/com/rest/api/repository/EmployeeRepository.java
@@ -5,34 +5,31 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.rest.api.pojo.Employee;
 
 public class EmployeeRepository {
 
-	private static List<Employee> empList = null;
+	private static List<Employee> empList = new ArrayList<Employee>();
 
 	public static List<Employee> getEmployeeDetails() {
 
 		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=restdb", "root",
-					"");
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://Natarajans-MacBook-Air.local:3306/test", "vijay",
+					"vijay");
 			System.out.println("connected");
 			Statement statement = conn.createStatement();
-
 			String sql = "select * from employee";
 			ResultSet rs = statement.executeQuery(sql);
 			while (rs.next()) {
-
 				Employee emp = new Employee();
 				int id = rs.getInt("empid");
 				String name = rs.getString("empname");
-
 				emp.setEmpId(id);
 				emp.setEmpName(name);
-
 				empList.add(emp);
 			}
 			rs.close();
@@ -44,16 +41,16 @@ public class EmployeeRepository {
 
 	public static Employee getEmployee(int id) {
 		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=restdb", "root",
-					"");
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://Natarajans-MacBook-Air.local:3306/test", "vijay",
+					"vijay");
 			System.out.println("connected");
 			String sql = "select * from employee where empId = ?";
 			PreparedStatement statement = conn.prepareStatement(sql);
 
 			statement.setInt(1, id);
 
-			ResultSet rs = statement.executeQuery(sql);
+			ResultSet rs = statement.executeQuery();
 			while (rs.next()) {
 
 				Employee emp = new Employee();
@@ -75,18 +72,58 @@ public class EmployeeRepository {
 	public static void createEmployee(Employee emp) {
 
 		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=restdb", "root",
-					"");
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://Natarajans-MacBook-Air.local:3306/test", "vijay",
+					"vijay");
 			System.out.println("connected");
-			Statement statement = conn.createStatement();
+			String sql = "insert into employee values (?,?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
 			int id = emp.getEmpId();
 			String name = emp.getEmpName();
-			statement.executeUpdate("insert into employee values(" + id + "," + name + ")");
+			statement.setInt(1, id);
+			statement.setString(2, name);
+			statement.executeUpdate();
 			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	public static void updateEmployee(Employee emp) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://Natarajans-MacBook-Air.local:3306/test", "vijay",
+					"vijay");
+			System.out.println("connected");
+			String sql = "update employee set empName = ? where empId = ?";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			int id = emp.getEmpId();
+			String name = emp.getEmpName();
+			statement.setString(1, name);
+			statement.setInt(2, id);
+			statement.executeUpdate();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteEmployee(int id) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://Natarajans-MacBook-Air.local:3306/test", "vijay",
+					"vijay");
+			System.out.println("connected");
+			String sql = "delete from employee where empId = ?";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, id);
+			statement.executeUpdate();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- RestApi/src/main/java/com/rest/api/resource/EmployeeResource.java
+++ RestApi/src/main/java/com/rest/api/resource/EmployeeResource.java
@@ -2,8 +2,10 @@ package com.rest.api.resource;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -32,5 +34,17 @@ public class EmployeeResource {
 	@Path("createemployee")
 	public void createEmployee(Employee emp) {
 		EmployeeRepository.createEmployee(emp);
+	}
+	
+	@PUT
+	@Path("updateemployee")
+	public void updateEmployee(Employee emp) {
+		EmployeeRepository.updateEmployee(emp);
+	}
+	
+	@DELETE
+	@Path("deleteemployee/{id}")
+	public void deleteEmployee(@PathParam("id") int id) {
+		EmployeeRepository.deleteEmployee(id);
 	}
 }
